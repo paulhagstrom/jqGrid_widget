b79b876570d7da19c74207e845096f7b7020f7a4
Paul Hagstrom <hagstrom@That-One-MacBook.local>
2010-08-21 16:20:22 -0400

    Just a bit of comment cleanup

--- app/controllers/jqgrid_widget_controller.rb
+++ app/controllers/jqgrid_widget_controller.rb
@@ -6,15 +6,7 @@ class JqgridWidgetController < ApplicationController
   # patches, and adds two shortcuts for standardizing/simplifying the addition of
   # jqgrid_widgets to the widget tree with proper communication channels open between them.
   
-  # Bring in Apotomo's controller methods, but redefine render_page_update_for to
-  # a) avoid reliance on Prototype, b) allow direct Javascript emission.
-  # Update: apotomo now has an official way to output js, so I can maybe avoid this.
-  # New version of Apotomo has moved ControllerMethods into Rails
   include Apotomo::Rails::ControllerMethods
-  # include JqueryApotomoControllerMethods
-  # Bring in a couple of things from jRails.  Probably it would be better to simply attach jRails in full,
-  # but for the moment there are only a couple of things that are needed for this to operate in jQuery alone.
-  # require 'jquery_apotomo_helper_methods'
   
   # The index method here is intended to be called via 'super' from the subclass.
   # Maybe someday it will do something
@@ -69,18 +61,11 @@ class JqgridWidgetController < ApplicationController
     cell_class = opts[:cell_class] || (resource_alias.pluralize + '_cell').camelize
     jqgrid_id = opts[:jqgrid_id] || (pfx + resource_alias.pluralize + '_list')
     x = widget(cell_class, widget_id, :_setup, opts.merge({:resource => resource, :jqgrid_id => jqgrid_id}))
-    # The API for widget seems to have changed as well, start state is now the third argument.
-    # x = widget(cell_class, :_setup, widget_id, opts.merge({:resource => resource, :jqgrid_id => jqgrid_id}))
-    # The API has changed, I no longer "watch" I guess, but rather respond_to_event.
     # This could probably be made more elegant.
     # All widgets watch themselves for cellClick, drawPanel, and deleteRecord events
     x.respond_to_event :cellClick, :from => x.name, :with => :_cell_click, :on => x.name
-    # x.watch(:cellClick, x.name, :_cell_click, x.name) #obsolete due to apotomo API change
-    # x.watch(:rowClick, x.name, :_row_click, x.name) # should be obsolete
     x.respond_to_event :drawPanel, :from => x.name, :with => :_draw_panel, :on => x.name
     x.respond_to_event :deleteRecord, :from => x.name, :with => :_delete_record, :on => x.name
-    # x.watch(:drawPanel, x.name, :_draw_panel, x.name) #obsolete due to apotomo API change
-    # x.watch(:deleteRecord, x.name, :_delete_record, x.name) #obsolete due to apotomo API change
     # Now the retrieval of Javascript data is an event?  Used to be address_to, which was basically deep linking.
     # Maybe I can go back to that, but let's see if this works.
     x.respond_to_event :fetchData, :on => x.name, :with => :_send_recordset, :from => x.name
@@ -104,14 +89,10 @@ class JqgridWidgetController < ApplicationController
     # Parents watch themselves for record selects and unselects, and send children into appropriate states.
     parent_cell.respond_to_event :recordSelected, :on => child_cell.name, :with => :_parent_selection, :from => parent_cell.name
     parent_cell.respond_to_event :recordUnselected, :on => child_cell.name, :with => :_parent_unselection, :from => parent_cell.name
-    # parent_cell.watch(:recordSelected, child_cell.name, :_parent_selection, parent_cell.name) #old apotomo API
-    # parent_cell.watch(:recordUnselected, child_cell.name, :_parent_unselection, parent_cell.name) #old apotomo API
     # Parents also watch children for record updates, and update themselves if one occurs.
     parent_cell.respond_to_event :recordUpdated, :on => parent_cell.name, :with => :_child_updated, :from => child_cell.name
-    # parent_cell.watch(:recordUpdated, parent_cell.name, :_child_updated, child_cell.name) #old apotomo API
     # Children watch themselves for recordChosen events, then send parent into update choice state
     child_cell.respond_to_event :recordChosen, :on => child_cell.name, :with => :_child_choice, :from => child_cell.name
-    # child_cell.watch(:recordChosen, child_cell.name, :_child_choice, child_cell.name) #old apotomo API
   end
     
   # A shortcut for embed_widget(parent, child = jqgrid_widget('resource', opts...))
